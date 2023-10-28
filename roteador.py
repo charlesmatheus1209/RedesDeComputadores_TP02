bb45b6e00e710404ebff33bbdcdf3e03cfe96de5
charlesmatheus1209 <charlesmatheus1209@gmail.com>
2023-10-28 17:15:28 -0300

    Incremento

--- roteador.py
+++ roteador.py
@@ -10,9 +10,26 @@ if(len(sys.argv) != 3):
 identificador = sys.argv[1]
 arquivoConfig = sys.argv[2]
 
-print(identificador)
-print(arquivoConfig)
+print("identificador: ", identificador)
+print("Arquivo: ", arquivoConfig)
 
-# socket = socket.socket(AF_INET, SOCK_DGRAM)
-# socket.bind(('',0))
+socket = socket.socket(AF_INET, SOCK_DGRAM)
+socket.bind((identificador,1234))
 
+while True:
+    msgFromServer = socket.recvfrom(1024)
+    mensagem = msgFromServer[0].decode('utf-8')
+    comando = mensagem[0]
+    
+    if(comando == 'C'):
+        print('Comando C')
+    elif(comando == 'D'):
+        print('Comando D')
+    elif(comando == 'T'):
+        print('Comando T')
+    elif(comando == 'I'):
+        print('Comando I')
+    elif(comando == 'E'):
+        print('Comando E')
+        
+    
